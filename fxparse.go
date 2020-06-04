76348106ed283ff337fa7cf59bae38cac18be033
Sergio Merino Hernández <sergiohdez94@gmail.com>
2020-06-04 21:57:38 +0200

    Improve verbosity
    This commit will improve the verbosity of the debug option and also do
    a better use of some skip functions of fxlex

--- fxparse.go
+++ fxparse.go
@@ -39,6 +39,10 @@ func (p *Parser) match(tT int) (t fxlex.Token, isMatch bool, e error) {
 	}
 
 	if t.GetTokType() != tT {
+		if t.GetTokType() == fxlex.TokEOF {
+			panic("unexpected EOF")
+		}
+
 		return t, false, nil
 	}
 
@@ -81,7 +85,7 @@ func (p *Parser) Prog() error {
 	case fxlex.TokFunc:
 		t, err = p.l.Lex()
 		p.pushTrace("\"func\"")
-		defer p.popTrace()
+		p.popTrace()
 
 		if err := p.Func(); err != nil {
 			return err
@@ -91,7 +95,7 @@ func (p *Parser) Prog() error {
 	case fxlex.TokEOF:
 		t, err = p.l.Lex()
 		p.pushTrace("\"EOF\"")
-		defer p.popTrace()
+		p.popTrace()
 	default:
 		p.errorf("%s:%d: syntax error: expected func or EOF, found %s",
 			p.l.GetFilename(), p.l.GetLineNumber(), t.GetTokType())
@@ -109,21 +113,26 @@ func (p *Parser) Func() error {
 		return err
 	}
 
-	_, isLCurl, err := p.match(fxlex.TokLCurl)
+	t, isLCurl, err := p.match(fxlex.TokLCurl)
 	if err != nil {
 		return err
 	} else if !isLCurl {
 		p.errorf("%s:%d: syntax error: macro bad definition",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntilAndLex(fxlex.TokRCurl)
-		return err
+		err = p.l.SkipUntil(fxlex.TokLCurl, fxlex.TokRCurl)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	if err := p.Body(); err != nil {
 		return err
 	}
 
-	_, isRCurl, err := p.match(fxlex.TokRCurl)
+	t, isRCurl, err := p.match(fxlex.TokRCurl)
 	if err != nil {
 		return err
 	} else if !isRCurl {
@@ -131,6 +140,9 @@ func (p *Parser) Func() error {
 			p.l.GetFilename(), p.l.GetLineNumber())
 		err = p.l.SkipUntilAndLex(fxlex.TokRCurl)
 		return err
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	return err
@@ -141,38 +153,51 @@ func (p *Parser) Head() error {
 	p.pushTrace("Head")
 	defer p.popTrace()
 
-	_, isID, err := p.match(fxlex.TokID)
+	t, isID, err := p.match(fxlex.TokID)
 	if err != nil {
 		return err
 	} else if !isID {
 		p.errorf("%s:%d: syntax error: macro bad definition",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokLCurl)
-		return err
+		err = p.l.SkipUntil(fxlex.TokLPar, fxlex.TokRPar, fxlex.TokComma)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("ID %s", t))
+		p.popTrace()
 	}
 
-	_, isLPar, err := p.match(fxlex.TokLPar)
+	t, isLPar, err := p.match(fxlex.TokLPar)
 	if err != nil {
 		return err
 	} else if !isLPar {
 		p.errorf("%s:%d: syntax error: macro bad definition",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokLCurl)
-		return err
+		err = p.l.SkipUntil(fxlex.TokRPar, fxlex.TokComma)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	if err := p.FormalPrms(); err != nil {
 		return err
 	}
 
-	_, isRPar, err := p.match(fxlex.TokRPar)
+	t, isRPar, err := p.match(fxlex.TokRPar)
 	if err != nil {
 		return err
 	} else if !isRPar {
 		p.errorf("%s:%d: syntax error: macro bad definition",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokLCurl)
+		err = p.l.SkipUntilAndLex(fxlex.TokRPar)
 		return err
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	return err
@@ -184,19 +209,27 @@ func (p *Parser) FormalPrms() error {
 	p.pushTrace("FormalPrms")
 	defer p.popTrace()
 
-	_, isTypeID, err := p.match(fxlex.TokID)
+	t, isTypeID, err := p.match(fxlex.TokID)
 	if err != nil || !isTypeID {
 		return err
+	} else {
+		p.pushTrace(fmt.Sprintf("TypeID %s", t))
+		p.popTrace()
 	}
 
-	_, isID, err := p.match(fxlex.TokID)
+	t, isID, err := p.match(fxlex.TokID)
 	if err != nil {
 		return err
 	} else if !isID {
 		p.errorf("%s:%d: syntax error: macro bad definition",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokRPar)
-		return err
+		err = p.l.SkipUntil(fxlex.TokRPar, fxlex.TokComma)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("ID %s", t))
+		p.popTrace()
 	}
 
 	return p.Prms()
@@ -208,29 +241,42 @@ func (p *Parser) Prms() error {
 	p.pushTrace("Prms")
 	defer p.popTrace()
 
-	_, isComma, err := p.match(fxlex.TokComma)
+	t, isComma, err := p.match(fxlex.TokComma)
 	if err != nil || !isComma {
 		return err
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
-	_, isTypeID, err := p.match(fxlex.TokID)
+	t, isTypeID, err := p.match(fxlex.TokID)
 	if err != nil {
 		return err
 	} else if !isTypeID {
 		p.errorf("%s:%d: syntax error: macro bad definition",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokRPar)
-		return err
+		err = p.l.SkipUntil(fxlex.TokRPar, fxlex.TokComma)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("TypeID %s", t))
+		p.popTrace()
 	}
 
-	_, isID, err := p.match(fxlex.TokID)
+	t, isID, err := p.match(fxlex.TokID)
 	if err != nil {
 		return err
 	} else if !isID {
 		p.errorf("%s:%d: syntax error: macro bad definition",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokRPar)
-		return err
+		err = p.l.SkipUntil(fxlex.TokRPar, fxlex.TokComma)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("ID %s", t))
+		p.popTrace()
 	}
 
 	return p.Prms()
@@ -253,16 +299,21 @@ func (p *Parser) Body() error {
 	case fxlex.TokID:
 		t, err = p.l.Lex()
 		p.pushTrace(fmt.Sprintf("ID %s", t))
-		defer p.popTrace()
+		p.popTrace()
 
-		_, isLPar, err := p.match(fxlex.TokLPar)
+		t, isLPar, err := p.match(fxlex.TokLPar)
 		if err != nil {
 			return err
 		} else if !isLPar {
 			p.errorf("%s:%d: syntax error: bad statement",
 				p.l.GetFilename(), p.l.GetLineNumber())
-			err = p.l.SkipUntilAndLex(fxlex.Semicolon)
-			return err
+			err = p.l.SkipUntil(fxlex.TokRPar, fxlex.TokComma, fxlex.Semicolon)
+			if err != nil {
+				return err
+			}
+		} else {
+			p.pushTrace(fmt.Sprintf("%s", t))
+			p.popTrace()
 		}
 
 		if err := p.Call(); err != nil {
@@ -272,31 +323,46 @@ func (p *Parser) Body() error {
 		return p.Body()
 	case fxlex.TokKey:
 		t, err = p.l.Lex()
-		p.pushTrace(fmt.Sprintf("Key %s", t))
-		defer p.popTrace()
 
 		switch t.GetLexeme() {
 		case "iter":
+			p.pushTrace(fmt.Sprintf("Key %s", t))
+			p.popTrace()
+
 			if err := p.Iter(); err != nil {
 				return err
 			}
 
 			return p.Body()
+		default:
+			p.errorf("%s:%d: syntax error: keyword unexpected",
+				p.l.GetFilename(), p.l.GetLineNumber())
+			err = p.l.SkipUntil(fxlex.TokLPar, fxlex.TokRPar, fxlex.TokComma)
+			if err != nil {
+				return err
+			}
 		}
 	case fxlex.TokLCurl:
 		t, _ = p.l.Lex()
+
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 
 		if err := p.Body(); err != nil {
 			return err
 		}
 
-		_, isRCurl, err := p.match(fxlex.TokRCurl)
+		t, isRCurl, err := p.match(fxlex.TokRCurl)
 		if err != nil {
 			return err
 		} else if !isRCurl {
 			p.errorf("%s:%d: syntax error: bad statement",
 				p.l.GetFilename(), p.l.GetLineNumber())
+			err = p.l.SkipUntilAndLex(fxlex.TokRCurl)
 			return err
+		} else {
+			p.pushTrace(fmt.Sprintf("%s", t))
+			p.popTrace()
 		}
 	default:
 		return err
@@ -311,28 +377,34 @@ func (p *Parser) Call() error {
 	p.pushTrace("Call")
 	defer p.popTrace()
 
-	_, isRPar, err := p.match(fxlex.TokRPar)
+	t, isRPar, err := p.match(fxlex.TokRPar)
 	if err != nil {
 		return err
-	}
-
-	if !isRPar {
+	} else if !isRPar {
 		if err := p.ArgsList(); err != nil {
 			return err
 		}
 
-		_, isRPar, err := p.match(fxlex.TokRPar)
+		t, isRPar, err := p.match(fxlex.TokRPar)
 		if err != nil {
 			return err
 		} else if !isRPar {
 			p.errorf("%s:%d: syntax error: bad statement",
 				p.l.GetFilename(), p.l.GetLineNumber())
-			err = p.l.SkipUntilAndLex(fxlex.Semicolon)
-			return err
+			err = p.l.SkipUntil(fxlex.Semicolon)
+			if err != nil {
+				return err
+			}
+		} else {
+			p.pushTrace(fmt.Sprintf("%s", t))
+			p.popTrace()
 		}
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
-	_, isSemicolon, err := p.match(fxlex.Semicolon)
+	t, isSemicolon, err := p.match(fxlex.Semicolon)
 	if err != nil {
 		return err
 	} else if !isSemicolon {
@@ -340,6 +412,9 @@ func (p *Parser) Call() error {
 			p.l.GetFilename(), p.l.GetLineNumber())
 		err = p.l.SkipUntilAndLex(fxlex.Semicolon)
 		return err
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	return err
@@ -363,9 +438,12 @@ func (p *Parser) Args() error {
 	p.pushTrace("Args")
 	defer p.popTrace()
 
-	_, isComma, err := p.match(fxlex.TokComma)
+	t, isComma, err := p.match(fxlex.TokComma)
 	if err != nil || !isComma {
 		return err
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	if err := p.Expr(); err != nil {
@@ -380,100 +458,140 @@ func (p *Parser) Iter() error {
 	p.pushTrace("Iter")
 	defer p.popTrace()
 
-	_, isLPar, err := p.match(fxlex.TokLPar)
+	t, isLPar, err := p.match(fxlex.TokLPar)
 	if err != nil {
 		return err
 	} else if !isLPar {
 		p.errorf("%s:%d: syntax error: iter (bad statement)",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokLCurl)
-		return err
+		err = p.l.SkipUntil(fxlex.TokLCurl, fxlex.TokRCurl, fxlex.TokRPar, fxlex.TokComma)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
-	_, isID, err := p.match(fxlex.TokID)
+	t, isID, err := p.match(fxlex.TokID)
 	if err != nil {
 		return err
 	} else if !isID {
 		p.errorf("%s:%d: syntax error: iter (bad statement)",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokLCurl)
-		return err
+		err = p.l.SkipUntil(fxlex.TokLCurl, fxlex.TokRCurl, fxlex.TokRPar, fxlex.TokComma)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("ID %s", t))
+		p.popTrace()
 	}
 
-	_, isDeclaration, err := p.match(fxlex.Declaration)
+	t, isDeclaration, err := p.match(fxlex.Declaration)
 	if err != nil {
 		return err
 	} else if !isDeclaration {
 		p.errorf("%s:%d: syntax error: iter (bad statement)",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokLCurl)
-		return err
+		err = p.l.SkipUntil(fxlex.TokLCurl, fxlex.TokRCurl, fxlex.TokRPar, fxlex.TokComma)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	if err := p.Expr(); err != nil {
 		return err
 	}
 
-	_, isComma, err := p.match(fxlex.TokComma)
+	t, isComma, err := p.match(fxlex.TokComma)
 	if err != nil {
 		return err
 	} else if !isComma {
 		p.errorf("%s:%d: syntax error: iter (bad statement)",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokLCurl)
-		return err
+		err = p.l.SkipUntil(fxlex.TokLCurl, fxlex.TokRCurl, fxlex.TokRPar, fxlex.TokComma)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	if err := p.Expr(); err != nil {
 		return err
 	}
 
-	_, isComma, err = p.match(fxlex.TokComma)
+	t, isComma, err = p.match(fxlex.TokComma)
 	if err != nil {
 		return err
 	} else if !isComma {
 		p.errorf("%s:%d: syntax error: iter (bad statement)",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokLCurl)
-		return err
+		err = p.l.SkipUntil(fxlex.TokLCurl, fxlex.TokRCurl, fxlex.TokRPar)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	if err := p.Expr(); err != nil {
 		return err
 	}
 
-	_, isRPar, err := p.match(fxlex.TokRPar)
+	t, isRPar, err := p.match(fxlex.TokRPar)
 	if err != nil {
 		return err
 	} else if !isRPar {
 		p.errorf("%s:%d: syntax error: iter (bad statement)",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntil(fxlex.TokLCurl)
-		return err
+		err = p.l.SkipUntil(fxlex.TokLCurl, fxlex.TokRCurl)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
-	_, isLCurl, err := p.match(fxlex.TokLCurl)
+	t, isLCurl, err := p.match(fxlex.TokLCurl)
 	if err != nil {
 		return err
 	} else if !isLCurl {
 		p.errorf("%s:%d: syntax error: iter (bad statement)",
 			p.l.GetFilename(), p.l.GetLineNumber())
-		err = p.l.SkipUntilAndLex(fxlex.TokRCurl)
-		return err
+		err = p.l.SkipUntil(fxlex.TokRCurl)
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	if err := p.Body(); err != nil {
 		return err
 	}
 
-	_, isRCurl, err := p.match(fxlex.TokRCurl)
+	t, isRCurl, err := p.match(fxlex.TokRCurl)
 	if err != nil {
 		return err
 	} else if !isRCurl {
 		p.errorf("%s:%d: syntax error: iter (bad statement)",
 			p.l.GetFilename(), p.l.GetLineNumber())
 		err = p.l.SkipUntilAndLex(fxlex.TokRCurl)
-		return err
+		if err != nil {
+			return err
+		}
+	} else {
+		p.pushTrace(fmt.Sprintf("%s", t))
+		p.popTrace()
 	}
 
 	return err
